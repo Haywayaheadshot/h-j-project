96ce7d24c30d72935ca48faf0dab4550dd78170d
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-12-04 07:31:29 -0600

    Keep file for testing boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,6 @@ import App from './App';
 // learn more: https://github.com/testing-library/jest-dom
 // import '@testing-library/jest-dom';
 
-
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
