306653df6266711e7cd403f916c99300cfe08345
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-12-04 07:05:37 -0600

    Delete default react code and put all styles in one dir

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+// import '@testing-library/jest-dom';
+
 
 test('renders learn react link', () => {
   render(<App />);
