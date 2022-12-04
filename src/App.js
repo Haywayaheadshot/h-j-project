791815871ffb9b4aa925bf3636b49b1e82fb6825
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-12-04 07:32:12 -0600

    Router editor to path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Editor from './pages/Editor';
 import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="app-header">
+          <NavBar />
+        </header>
+        <main className="app-main">
+          <Routes>
+            <Route exact path="/" element={<Editor />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+
   );
 }
 
