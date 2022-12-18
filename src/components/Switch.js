a7e4fd692dc785e5dc7973c7a35d26edc3c39488
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-12-18 14:37:13 -0600

    Add switch component

---
+++ src/components/Switch.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import '../styles/App.css';
+import '../styles/switch.css';
+
+function Switch() {
+  const [day, night] = useState(false);
+  const { body } = document;
+  return (
+    <div>
+      <label htmlFor="switch-app" className="switch-label-app">
+        {!day && (
+          body.classList.add('day'),
+          body.classList.remove('night')
+        )}
+        {day && (
+          body.classList.replace('day', 'night')
+        )}
+        <input onClick={() => night((day) => !day)} className="switch-input-app" id="switch-app" type="checkbox" />
+        <span className="switch-slider-app" />
+      </label>
+    </div>
+  );
+}
+
+export default Switch;
